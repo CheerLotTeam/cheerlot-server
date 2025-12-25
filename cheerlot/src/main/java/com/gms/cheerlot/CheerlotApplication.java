b7393281b129e38befbfdaf90e73667322adbeb8
seongm1n <0411tjdals34@gmail.com>
2025-12-25 22:52:12 +0900

    initial commit

---
+++ cheerlot/src/main/java/com/gms/cheerlot/CheerlotApplication.java
@@ -0,0 +1,13 @@
+package com.gms.cheerlot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheerlotApplication.class, args);
+	}
+
+}
