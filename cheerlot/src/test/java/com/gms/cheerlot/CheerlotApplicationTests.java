bce3fbf70c5598a4a583b070a559b0a6d0347b46
seongm1n <0411tjdals34@gmail.com>
2026-03-13 14:35:36 +0900

    fix(test): Mock CacheLoader in SpringBootTest to avoid Notion API call (#15)

--- cheerlot/src/test/java/com/gms/cheerlot/CheerlotApplicationTests.java
+++ cheerlot/src/test/java/com/gms/cheerlot/CheerlotApplicationTests.java
@@ -1,10 +1,15 @@
 package com.gms.cheerlot;
 
+import com.gms.cheerlot.cache.CacheLoader;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CheerlotApplicationTests {
+
+	@MockitoBean
+	private CacheLoader cacheLoader;
 
 	@Test
 	void contextLoads() {
